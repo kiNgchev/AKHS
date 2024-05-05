bfa8d5b760b730ca6d52607f33da6097392db604
KiNgchev <ketone.git@gmail.com>
2024-05-05 16:42:43 +0300

    Add akhs-core and akhs-youtube modules

---
+++ akhs-youtube/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "net.kingchev"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
