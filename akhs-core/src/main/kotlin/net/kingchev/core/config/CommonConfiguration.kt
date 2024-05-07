90bfc0bf934ce932d22c1cc6437344749d5739b8
KiNgchev <ketone.git@gmail.com>
2024-05-07 22:03:28 +0300

    Updating common configuration
    Updating liquibase bean configuration

--- akhs-core/src/main/kotlin/net/kingchev/core/config/CommonConfiguration.kt
+++ akhs-core/src/main/kotlin/net/kingchev/core/config/CommonConfiguration.kt
@@ -1,11 +1,12 @@
 package net.kingchev.core.config
 
+import liquibase.integration.spring.SpringLiquibase
 import net.kingchev.core.kafka.KafkaConfiguration
-import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
+import javax.sql.DataSource
 
 @Configuration
 @Import(
@@ -15,8 +16,10 @@ import org.springframework.context.annotation.Import
 class CommonConfiguration {
     //Configuring liquibase spring bean
     @Bean
-    fun liquibase(): LiquibaseProperties {
-        val props = LiquibaseProperties()
+    fun liquibase(datasource: DataSource): SpringLiquibase {
+        val props = SpringLiquibase()
+        //setting the datasource
+        props.dataSource = datasource
         //setting the path to changelog
         props.changeLog = "classpath:/db/changelog/db.changelog-master.yaml"
         return props
