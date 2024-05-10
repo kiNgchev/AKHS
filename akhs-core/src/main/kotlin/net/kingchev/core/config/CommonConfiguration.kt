6a97e23f4d1886f6a01485323a901f77b90a9af8
KiNgchev <ketone.git@gmail.com>
2024-05-10 16:01:08 +0300

    Updating configurations

--- akhs-core/src/main/kotlin/net/kingchev/core/config/CommonConfiguration.kt
+++ akhs-core/src/main/kotlin/net/kingchev/core/config/CommonConfiguration.kt
@@ -1,19 +1,30 @@
 package net.kingchev.core.config
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import liquibase.integration.spring.SpringLiquibase
 import net.kingchev.core.kafka.KafkaConfiguration
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
+import org.springframework.scheduling.annotation.EnableScheduling
 import javax.sql.DataSource
 
 @Configuration
 @Import(
     KafkaConfiguration::class
 )
+@EnableScheduling
+@ConfigurationPropertiesScan(basePackages = ["net.kingchev"])
+@ComponentScan(basePackages = ["net.kingchev"])
 @EnableConfigurationProperties
 class CommonConfiguration {
+    @Bean
+    fun gson(): Gson = GsonBuilder().create()
+
     //Configuring liquibase spring bean
     @Bean
     fun liquibase(datasource: DataSource): SpringLiquibase {
