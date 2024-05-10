8c03a9ebcd00ad2c394c96bddf2ba0bcd52bebe9
KiNgchev <ketone.git@gmail.com>
2024-05-10 16:06:32 +0300

    Updating CrossPostingMessage

--- akhs-core/src/main/kotlin/net/kingchev/core/model/CrossPostingMessage.kt
+++ akhs-core/src/main/kotlin/net/kingchev/core/model/CrossPostingMessage.kt
@@ -1,11 +1,8 @@
 package net.kingchev.core.model
 
-import java.time.OffsetDateTime
-
 data class CrossPostingMessage(
     var id: String,
-    var title: String?,
+    var source: String,
+    var title: String,
     var content: String?,
-    var data: List<Attachment>,
-    var timestamp: OffsetDateTime?
 )
