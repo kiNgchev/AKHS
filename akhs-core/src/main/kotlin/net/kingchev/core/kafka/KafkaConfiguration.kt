b4060936386512fe74a7ab3d165bee5ce6e68343
KiNgchev <ketone.git@gmail.com>
2024-05-10 16:10:30 +0300

    Updating kafka configuration
    Creating topics

--- akhs-core/src/main/kotlin/net/kingchev/core/kafka/KafkaConfiguration.kt
+++ akhs-core/src/main/kotlin/net/kingchev/core/kafka/KafkaConfiguration.kt
@@ -3,14 +3,19 @@ package net.kingchev.core.kafka
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.annotation.EnableKafka
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
 import org.springframework.kafka.listener.ContainerProperties.AckMode
 import java.time.Duration
+
+const val CROSS_POSTING_TOPIC = "akhs.cross-posting"
+const val TWITCH_NOTIFICATION = "akhs.twitch.notification"
+const val YOUTUBE_NOTIFICATION = "akhs.youtube.notification"
 
 @EnableKafka
 @Configuration
 class KafkaConfiguration(kafkaProperties: KafkaProperties) {
     init {
-        kafkaProperties.bootstrapServers = listOf("localhost:29092")
+        kafkaProperties.bootstrapServers = listOf("localhost:9092")
         kafkaProperties.consumer.apply { consumer() }
         kafkaProperties.producer.apply { producer() }
         kafkaProperties.listener.apply { listener() }
@@ -24,8 +29,8 @@ class KafkaConfiguration(kafkaProperties: KafkaProperties) {
             clientId = "akhs"
             maxPollRecords = 1
             fetchMaxWait = Duration.ofMillis(36000)
-            keyDeserializer = org.apache.kafka.common.serialization.StringSerializer::class.java
-            valueDeserializer = org.apache.kafka.common.serialization.StringSerializer::class.java
+            keyDeserializer = org.apache.kafka.common.serialization.StringDeserializer::class.java
+            valueDeserializer = org.apache.kafka.common.serialization.StringDeserializer::class.java
         }
 
         private val producer: KafkaProperties.Producer.() -> Unit = {
